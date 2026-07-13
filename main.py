2f8fc205e786fe2c7e0da8d7889bb21d6702e032
Kairo47 <aviii7y.work@gmail.com>
2026-07-13 10:45:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+from core.Astroz import Astroz
+import asyncio
+import jishaku, cogs
+import discord
+
+os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True"
+os.environ["JISHAKU_HIDE"] = "True"
+os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
+os.environ["JISHAKU_FORCE_PAGINATOR"] = "True"
+
+client = Astroz()
+
+
+@client.event
+async def on_ready():
+    print("Loaded & Online!")
+    print(f"Logged in as: {client.user}")
+    print(f"Connected to: {len(client.guilds)} guilds")
+    print(f"Connected to: {len(client.users)} users")
+    try:
+        synced = await client.tree.sync()
+        print(f"Synced {len(synced)} slash commands")
+    except Exception as e:
+        print(e)
+
+
+async def main():
+    async with client:
+        await client.load_extension("cogs")
+        await client.load_extension("jishaku")
+        await client.start(os.getenv("TOKEN"))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
